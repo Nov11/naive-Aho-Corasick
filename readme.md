8df3c166c523fc8fe0c4bf605535753fa49f0786
Nov11 <529079634@qq.com>
2018-10-13 21:17:09 +0800

    + add a larger test case and benchmark against another aho-corasick implementation

--- readme.md
+++ readme.md
@@ -1,2 +1,21 @@
 [![Build Status](https://travis-ci.org/Nov11/naive-Aho-Corasick.svg?branch=master)](https://travis-ci.org/Nov11/naive-Aho-Corasick)
 [![Coverage Status](https://coveralls.io/repos/github/Nov11/naive-Aho-Corasick/badge.svg?branch=master)](https://coveralls.io/github/Nov11/naive-Aho-Corasick?branch=master)
+
+#### Benchmark
+File: AhoCorasickTest leveraging JUnitBenchmarks
+* Input: 
+    - text string: kjv bible
+    - word list: words in this bible
+* Output:
+    - matched words
+
+```
+AhoCorasickTest.robert_bor_aho_corasick: [measured 10 out of 15 rounds, threads: 1 (sequential)]
+ round: 0.31 [+- 0.05], round.block: 0.00 [+- 0.00], round.gc: 0.00 [+- 0.00], GC.calls: 8, GC.time: 0.48, time.total: 5.65, time.warmup: 2.52, time.bench: 3.13
+AhoCorasickTest.match: [measured 10 out of 15 rounds, threads: 1 (sequential)]
+ round: 0.00 [+- 0.00], round.block: 0.00 [+- 0.00], round.gc: 0.00 [+- 0.00], GC.calls: 0, GC.time: 0.00, time.total: 0.00, time.warmup: 0.00, time.bench: 0.00
+AhoCorasickTest.randomMatchTest: [measured 10 out of 15 rounds, threads: 1 (sequential)]
+ round: 0.28 [+- 0.00], round.block: 0.00 [+- 0.00], round.gc: 0.00 [+- 0.00], GC.calls: 0, GC.time: 0.00, time.total: 4.32, time.warmup: 1.49, time.bench: 2.83
+
+Process finished with exit code 0
+```
--- src/test/java/io/nov11/AhoCorasickTest.java
+++ src/test/java/io/nov11/AhoCorasickTest.java
@@ -1,15 +1,53 @@
 package io.nov11;
 
+import com.carrotsearch.junitbenchmarks.BenchmarkRule;
+import org.ahocorasick.trie.Emit;
+import org.ahocorasick.trie.Trie;
 import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TestRule;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.Set;
-
-import static org.junit.Assert.*;
+import java.util.stream.Collectors;
 
 public class AhoCorasickTest {
+    @Rule
+    public TestRule benchmarkRun = new BenchmarkRule();
+
+    private static String content;
+
+    private static List<String> words;
+
+    private static AhoCorasick ahoCorasick;
+
+    private static Trie trieFromRobertBor;
+
+    @BeforeClass
+    public static void init() throws Exception {
+        String fileName = "src/test/resources/kjvdat.txt";
+        String wordList = "src/test/resources/words";
+
+        byte[] contentBytes = Files.readAllBytes(Paths.get(fileName));
+        content = new String(contentBytes, StandardCharsets.UTF_8).toLowerCase();
+        words = Files.readAllLines(Paths.get(wordList));
+        ahoCorasick = new AhoCorasick(words);
+
+
+        Trie.TrieBuilder builder = Trie.builder();
+        for (String s : words) {
+            builder.addKeyword(s);
+        }
+        trieFromRobertBor = builder.build();
+    }
 
     @Test
     public void match() {
@@ -26,5 +64,18 @@ public class AhoCorasickTest {
         Assert.assertTrue(ret.contains("he"));
         Assert.assertTrue(ret.contains("hers"));
 
+    }
+
+    @Test
+    public void randomMatchTest() {
+        Set<String> ret = ahoCorasick.match(content);
+        Assert.assertEquals(words.size(), ret.size());
+    }
+
+    @Test
+    public void robert_bor_aho_corasick() {
+        Collection<Emit> ret = trieFromRobertBor.parseText(content);
+        Set<String> result = ret.stream().map(Emit::getKeyword).collect(Collectors.toSet());
+        Assert.assertEquals(words.size(), result.size());
     }
 }
